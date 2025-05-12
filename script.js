577f061f9024f974963e3f422f929c6d8275bce9
rafsaniShazid <rafsanishazid@gmail.com>
2025-05-12 17:58:46 +0600

    prototype

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const roles = ["Web Developer", "Problem Solver", "SEO Expert",
+    "Android Developer"];
+let idx = 0;
+const roleText = document.getElementById("role-text");
+
+setInterval(() => {
+  roleText.style.opacity = 0;
+  setTimeout(() => {
+    roleText.textContent = roles[idx];
+    roleText.style.opacity = 1;
+    idx = (idx + 1) % roles.length;
+  }, 500); // fade-out duration
+}, 2500); // change every 2 seconds
